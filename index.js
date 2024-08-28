4c576642a59a80c5705a505f320119d9e1d5ed7c
joaodejesus2211 <165792136+joaodejesus2211@users.noreply.github.com>
2024-08-28 07:59:25 -0300

    joao

---
+++ index.js
@@ -0,0 +1,16 @@
+var nome = 'Jonh';
+let idade = 25;
+const pi = 3.14;
+
+let a = 5;
+let b = 10; 
+let soma = a + b;
+
+let a = 5; 
+let b = 10;
+function mutiplica (x , y){
+    return x * y;
+}
+let produto = mutiplica (a , b);
+
+let frutas = ['apple']
